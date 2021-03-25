625daf3f9f16aa6140ef9393b14a33b423217b8d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-03-25 16:15:27 +0900

    Day 1st.

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() {
+    // Day 1st. Lecture
+    printf("Hello World! \n");
+    printf("123\n456\n789\n");
+    printf("シングルクオーテーション:\'ダブルクオーテーション:\"\n");
+    printf("\t円マーク¥\n");
+    printf("こんにちは。私の名前は%sです。\n年齢は%d歳です。\n", "山田太郎", 20);
+    printf("イニシャルは、%cです。\n", 'Y');
+    printf("%f + %f = %f\n", 1.2, 2.7, 1.2 + 2.7);
+    printf("ABC");
+    printf("DEF\n");
+    
+    // Day 1st. Exercise
+    printf("I'm Kenta Yamaguchi\n");
+    printf("123\n456\n789\n");
+    printf("%d + %d = %d\n", 1, 1, 1 + 1);
+    printf("%d + %d = %d\n", 2, 3, 2 + 3);
+    printf("%d + %d + %d = %d\n", 1, 2, 3, 1 + 2 + 3);
+}
